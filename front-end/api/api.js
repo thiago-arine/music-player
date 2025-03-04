806a18f15a42e13c55ac2dbc7d5269bcb2d7b007
Thiago Arine <thiago.arine@aluno.ifsp.edu.br>
2025-03-04 00:00:49 -0300

    process env fixed

--- front-end/api/api.js
+++ front-end/api/api.js
@@ -2,8 +2,9 @@
 import "dotenv/config";
 import axios from "axios";
 
-const NODE_ENV = process.env.NODE_ENV;
-const URL = NODE_ENV === "development" ? "http://localhost:3000/api" : "/api";
+// const NODE_ENV = process.env.NODE_ENV;
+// const URL = "http://localhost:3000/api";
+const URL = "https://music-player-3nt1.onrender.com/api";
 
 const responseArtists = await axios.get(`${URL}/artists`);
 const responseSongs = await axios.get(`${URL}/songs`);
